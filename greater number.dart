fd4bd3336827d269e5b26291453da76d6bd61ccc
DawnflashyXavier <kadirivictor93@gmail.com>
2023-07-10 01:39:19 +0100

    assignment

--- greater number.dart
+++ greater number.dart
@@ -5,15 +5,8 @@ void main(List<String> args) {
   int? second =int.parse(stdin.readLineSync()!);
   int? third =int.parse(stdin.readLineSync()!);
 
-  if(first>second+third){
-    print("$first is the greatest");
-  }
-  else{
-    (second>first +third);
-    print("$second is the greatest");
-  }
-  else{
-    (third>second+third);
-    print("$third is the greatest");
-  }
+  int maxValue= findMaxValue(first, second, third);
+
 }
+
+Future<int> findMaxValue(int first, int secon
--- positive or negative.dart
+++ positive or negative.dart
@@ -4,7 +4,9 @@ void main(List<String> args) {
   int? number = int.parse(stdin.readLineSync()!);
   if(number < 0){
     print("It is a negative number");
-  }else {
+  }else if(number > 0){
     print("It is a positive number");
-  }
+  }else {
+    print("The number is 0");
+  };
 }
