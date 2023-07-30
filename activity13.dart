695420038f87addf8c1db2d5e6e88e1a63268033
DawnflashyXavier <kadirivictor93@gmail.com>
2023-07-31 00:33:02 +0100

    new

--- activity13.dart
+++ activity13.dart
@@ -3,9 +3,10 @@ import 'dart:math';
 void main(List<String> args) {
   stdout.write("Guess what number is on my mind (from 1-10): ");
   int? num = int.parse(stdin.readLineSync()!);
+  if()
 
-  var random = Random();
-  int randomNumber = random.nextInt(11);
-
-  if  
+    
 }
+
+void random = Random();
+  int randomNumber = random.nextInt(11);
